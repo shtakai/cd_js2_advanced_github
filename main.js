64c01d0b6c770651807ae0aa1df9ac7441b5a03f
Alyson Takai <shtakai@gmail.com>
2016-07-06 19:11:25 -0700

    promise

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 $('#button').on('click', function(e){
-  $.get('https://api.github.com/users/sashimi', displayName)
+  var data = new Promise(function(resolve, reject){
+    getUserData(resolve, reject);
+  });
+  data.then(function(data){
+    console.log('just get data');
+    $('#info').text(JSON.stringify(data));
+  });
+  console.log('end');
 });
 
-function displayName(data){
-  console.log(data);
-  $('#info').text(JSON.stringify(data));
+function getUserData(resolve, reject){
+  console.log(1);
+  setTimeout(function(){
+    $.get('https://api.github.com/users/sashimi', function(json){
+      resolve(json);
+  });
+  }, 5000);
+  console.log(2);
 }
 
 
